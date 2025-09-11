776fc0578949fbeea4566fa51f856a7cb9064086
nnminh322 <nnminh322@gmail.com>
2025-09-11 22:38:56 +0700

    udate skeleton codebase

---
+++ DataAgent/README.md
@@ -0,0 +1,1 @@
+copyright &copy; nnminh322@gmail.com
---
+++ DataAgent/llm/llm_model.py
@@ -0,0 +1,27 @@
+from langchain_openai import ChatOpenAI
+
+
+class LLM_standard_OpenAI_API:
+    def __init__(self):
+        self.llm = ChatOpenAI(
+            model="openai/gpt-4o",
+            api_key="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",
+            base_url="https://models.github.ai/inference",
+        )
+    
+    def get_llm(self):
+        return self.llm
+    
+
+def main():
+    llm = LLM_standard_OpenAI_API().get_llm()
+    input = "Hello, Do you known HUST?"
+    output = llm.invoke(input=input)
+    print(output)
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
