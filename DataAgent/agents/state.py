24fa85cb7a7df2777633b12f7142c1f616910b2f
nnminh322 <nnminh322@gmail.com>
2025-09-11 23:00:04 +0700

    define MessageState

---
+++ DataAgent/agents/state.py
@@ -0,0 +1,11 @@
+from typing import TypedDict, Literal, Dict, List, Any, Optional
+
+class MessageState(TypedDict):
+    SQL_format: Literal['postgres', 'mssql', 'mysql']
+    question: str
+    db_description: str
+    chat_history: List[Dict[str,str]]
+    SQL_statement: Optional[str]
+    SQL_error_results: Optional[str]
+    SQL_num_retry: int
+    SQL_data_results: Optional[List[Dict[str, Any]]]
