83f1ffa00848506f45dff78ce3703b3198f6f7d0
Nguyen Nhat Minh <nnminh322@gmail.com>
2025-10-06 17:59:51 +0700

    cache

--- functional.py
+++ functional.py
@@ -33,7 +33,7 @@ def chat_sql(question: str):
     state = MessageState()
     state["question"] = question
     try:
-        out_state = app.invoke(state)
+        out_state = app.invoke(state, config={"recursion_limit": 100})
     except Exception as e:
         return {"error": str(e)}
     return {"results": out_state["SQL_data_results"]}
